3b36909ece32a5023a3500d101dea89b7350469d
wetooa <derpkidyt@gmail.com>
2024-03-16 11:05:16 +0800

    pushed to repo

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from dotenv import load_dotenv
 import os
 
+
+load_dotenv()
+
+
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
